e8b0c1698413fe4baf94d7ff449cb6e09882a4c2
mmd-af <thepermonica@gmail.com>
2025-11-28 16:46:54 +0200

    fix: staging

--- docker/staging/entrypoint.sh
+++ docker/staging/entrypoint.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 set -e
 
-# This script is executed every time the container starts.
-# It's a good place to run tasks that should happen at startup.
-
-# For a Laravel application, it's a good practice to cache the configuration,
-# routes, and views for better performance in production.
-# The `optimize` command handles this.
-echo "Optimizing Laravel application..."
-php artisan optimize
-
 # The "$@" is a shell variable that means "all the arguments".
 # In this case, it will be the command passed by Docker, e.g., "php-fpm".
 # 'exec' replaces the current shell process with the new one, which is a best practice.
