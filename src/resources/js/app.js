da6c2555e694be45da019ee715ea5839375acd8d
mmd-af <thepermonica@gmail.com>
2025-11-29 17:00:32 +0200

    feat: Added template and dependencies

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+import './lib/wow/wow.min.js';
+import './lib/easing/easing.min.js';
+import './lib/waypoints/waypoints.min.js';
+import './lib/owlcarousel/owl.carousel.min.js';
+import './lib/counterup/counterup.min.js';
+import './main.js';
