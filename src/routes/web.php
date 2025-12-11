02afd9a50cbc80dd6c8b9d03f58eb9f4eae32e3c
Mohammad Afshar <99553574+mmd-af@users.noreply.github.com>
2025-12-11 12:44:40 +0000

    refactor: change the landing page

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Livewire\EventShow;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('components.layouts.app');
 });
 
 Route::get('/event/{slug}', EventShow::class)->name('event.show');
