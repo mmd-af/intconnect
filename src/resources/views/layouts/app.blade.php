d1063d05990bb14fa171627832c0d858f7a6fc98
mmd-af <thepermonica@gmail.com>
2026-01-06 19:19:21 +0100

    refactor: Remove extra files and change theme color

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -191,13 +191,6 @@
                 <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
                     &copy; <a class="fw-semi-bold" href="#!">intconnect.ro</a>, All Right Reserved.
                 </div>
-                <div class="col-md-6 text-center text-md-end">
-                    <!--/*** This template is free as long as you keep the below author’s credit link/attribution link/backlink. ***/-->
-                    <!--/*** If you'd like to use the template without the below author’s credit link/attribution link/backlink, ***/-->
-                    <!--/*** you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". ***/-->
-                    Designed By <a class="fw-semi-bold" href="https://m-afshar.de" target="_blank">mdd-af</a>. Distributed by
-                    <a class="fw-semi-bold" href="https://themewagon.com">ThemeWagon</a>
-                </div>
             </div>
         </div>
     </div>
